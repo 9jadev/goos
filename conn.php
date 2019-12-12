97212f018f2e7d33220731ba5e41269ccd9992e7
Soloski123 <ahambasolomon800@gmail.com>
2019-12-12 03:11:40 -0800

    second fix of conn on digtal ocean

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
  
 //MySQLi Procedural
-$conn = mysqli_connect("localhost", "admin", "7fe0bb3f07e5e34ad95e78303dac0e005a31c323def93eb2","transcar_info");
+$conn = mysqli_connect("localhost", "admin", "7fe0bb3f07e5e34ad95e78303dac0e005a31c323def93eb2","transcar_client");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
