633b22388fb27569b940600bc6952898b97e3955
Soloski123 <ahambasolomon800@gmail.com>
2019-12-12 03:08:36 -0800

    dg ocean

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
  
 //MySQLi Procedural
-$conn = mysqli_connect("localhost","root","","transcar_info");
+$conn = mysqli_connect("localhost", "admin", "7fe0bb3f07e5e34ad95e78303dac0e005a31c323def93eb2","transcar_info");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
 <?php
 //connect to mysql database
-$con = mysqli_connect("localhost", "root", "", "transcar_client") or die("Error " . mysqli_error($con));
+$con = mysqli_connect("localhost", "admin", "7fe0bb3f07e5e34ad95e78303dac0e005a31c323def93eb2", "transcar_client") or die("Error " . mysqli_error($con));
 ?>
